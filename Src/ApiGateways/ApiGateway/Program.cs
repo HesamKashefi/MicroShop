591c980478295a4a9aa43374a05db6375ce7a165
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-07-23 15:24:36 +0330

    Trust Certificates in apigateway

--- Src/ApiGateways/ApiGateway/Program.cs
+++ Src/ApiGateways/ApiGateway/Program.cs
@@ -1,10 +1,17 @@
 using Common;
+using System.Net;
+
+ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
 
 await Extensions.RunInLoggerAsync(async () =>
 {
     var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddReverseProxy()
-        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+        .ConfigureHttpClient((context, handler) =>
+        {
+            handler.SslOptions.RemoteCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+        });
     var app = builder.Build();
     app.MapReverseProxy();
     await app.RunAsync();
