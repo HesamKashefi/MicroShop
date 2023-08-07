758f4975201960025810d72501539357252c6574
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-08-07 14:17:14 +0330

    login code fixed

--- Src/View/MicroShop.Admin/ClientApp/src/modules/auth/login/login.component.ts
+++ Src/View/MicroShop.Admin/ClientApp/src/modules/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from 'src/modules/shared/services/config.service';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +9,19 @@ import { OAuthService } from 'angular-oauth2-oidc';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private configService: ConfigService, private oauthService: OAuthService) {
+  constructor(private configService: ConfigService, private oauthService: OAuthService, private router: Router) {
   }
 
   ngOnInit(): void {
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
-    this.oauthService.tryLoginCodeFlow();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin()
+      .then(loggedIn => {
+        if (loggedIn) {
+          this.router.navigateByUrl("/");
+        }
+      });
   }
 
   onLogin() {
     this.oauthService.initCodeFlow();
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
   }
 }
