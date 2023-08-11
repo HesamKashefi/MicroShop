1036baab63fc715591922d9c9a634fb39e98d249
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-08-11 14:14:29 +0330

    Added login/logout to View

---
+++ Src/View/MicroShop.View/Controller/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroShop.View.Controller
+{
+    [Route("[Controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("SignIn")]
+        public ActionResult SignIn()
+        {
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = "/",
+                IsPersistent = true
+            }, OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("SignOut")]
+        public async Task<IActionResult> SignOutAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //signout from identity.api
+            //await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            return Redirect("/");
+        }
+    }
+}
