555d8f598a5ea0391529691ed0b7d1f9f6e847e4
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-08-19 10:44:50 +0330

    Implemented OrdersRepository

--- Src/Services/Orders/Orders.Domain/Contracts/IOrdersRepository.cs
+++ Src/Services/Orders/Orders.Domain/Contracts/IOrdersRepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IOrdersRepository
     {
-        Task<Order> GetOrderAsync(int orderId);
+        Task<Order?> GetOrderAsync(int orderId);
         Task<Order[]> GetBuyerOrdersAsync(int buyerId);
         Task SaveOrderAsync(Order order);
     }
--- Src/Services/Orders/Orders.Persistence/Repositories/OrdersRepository.cs
+++ Src/Services/Orders/Orders.Persistence/Repositories/OrdersRepository.cs
@@ -1,23 +1,44 @@
-﻿using Orders.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Domain;
 using Orders.Domain.Contracts;
 
 namespace Orders.Persistence.Repositories
 {
     public class OrdersRepository : IOrdersRepository
     {
-        public Task<Order[]> GetBuyerOrdersAsync(int buyerId)
+        private readonly OrdersDb _db;
+
+        public OrdersRepository(OrdersDb db)
         {
-            throw new NotImplementedException();
+            _db = db;
         }
 
-        public Task<Order> GetOrderAsync(int orderId)
+        public async Task<Order[]> GetBuyerOrdersAsync(int buyerId)
         {
-            throw new NotImplementedException();
+            return await _db.Orders
+                .Where(order => order.BuyerId == buyerId)
+                .AsNoTracking()
+                .ToArrayAsync();
         }
 
-        public Task SaveOrderAsync(Order order)
+        public async Task<Order?> GetOrderAsync(int orderId)
         {
-            throw new NotImplementedException();
+            return await _db.Orders
+                .AsNoTracking()
+                .FirstOrDefaultAsync(order => order.BuyerId == orderId);
+        }
+
+        public async Task SaveOrderAsync(Order order)
+        {
+            if (order.Id == 0)
+            {
+                _db.Orders.Add(order);
+            }
+            else
+            {
+                _db.Entry(order).State = EntityState.Modified;
+            }
+            await _db.SaveChangesAsync();
         }
     }
 }
