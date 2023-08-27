260a6822a2ef04b9fed5a9783d2ea4f5a92c4373
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-08-27 11:00:26 +0330

    Added a comment for RabbitMQ Connection

--- Src/BuildingBlocks/EventBus.RabbitMq/IRabbitMQPersistentConnection.cs
+++ Src/BuildingBlocks/EventBus.RabbitMq/IRabbitMQPersistentConnection.cs
@@ -3,8 +3,15 @@ using RabbitMQ.Client;
 
 namespace EventBus.RabbitMq
 {
+    /// <summary>
+    /// Holds a connection to RabbitMQ
+    /// </summary>
     public interface IRabbitMQPersistentConnection : IEventBusPersistentConnection
     {
+        /// <summary>
+        /// Creates a RabbitMQ Model
+        /// </summary>
+        /// <returns>RabbitMQ Model</returns>
         IModel CreateModel();
     }
 }
