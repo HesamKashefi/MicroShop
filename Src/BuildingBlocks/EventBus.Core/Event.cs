79f0ed877181df503d9c275a830066b23aed206c
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-08-28 12:18:10 +0330

    Added EventId to Event

--- Src/BuildingBlocks/EventBus.Core/Event.cs
+++ Src/BuildingBlocks/EventBus.Core/Event.cs
@@ -1,7 +1,18 @@
 ﻿namespace EventBus.Core
 {
+    /// <summary>
+    /// Represents an Event in a service
+    /// </summary>
     public abstract class Event
     {
+        /// <summary>
+        /// Id of the event
+        /// </summary>
+        public Guid EventId { get; private set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// The time of event
+        /// </summary>
         public DateTime CaptureTime { get; init; } = DateTime.Now;
     }
 }
