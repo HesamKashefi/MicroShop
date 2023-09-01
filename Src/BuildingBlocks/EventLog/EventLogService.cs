990dd1282844c931a32e3ad4f949194f2b328aec
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-09-01 09:33:34 +0330

    Better Implementation for event log service

--- Src/BuildingBlocks/EventLog/EventLogService.cs
+++ Src/BuildingBlocks/EventLog/EventLogService.cs
@@ -1,62 +1,84 @@
 ﻿using EventBus.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace EventLog
 {
     public class EventLogService : IEventLogService
     {
+        #region Dependencies
         private readonly EventLogContext _eventLogContext;
+        private readonly IEventBus _eventBus;
+        private readonly IEventBusSubscriptionManager _eventBusSubscriptionManager;
+        private readonly ILogger<EventLogService> _logger;
+        #endregion
 
-        public EventLogService(EventLogContext eventLogContext)
+        #region Ctor
+        public EventLogService(
+           EventLogContext eventLogContext,
+           IEventBus eventBus,
+           IEventBusSubscriptionManager eventBusSubscriptionManager,
+           ILogger<EventLogService> logger)
         {
             _eventLogContext = eventLogContext;
+            _eventBus = eventBus;
+            _eventBusSubscriptionManager = eventBusSubscriptionManager;
+            _logger = logger;
         }
+        #endregion
 
-        public async Task AddEventAsync(Event @event, CancellationToken cancellationToken)
+        #region Implementation of IEventLogService
+        public async Task AddEventAsync(Event @event, IDbContextTransaction transaction, CancellationToken cancellationToken)
         {
+            _eventLogContext.Database.UseTransaction(transaction.GetDbTransaction());
             _eventLogContext.Events.Add(new EventLogEntry(@event));
             await _eventLogContext.SaveChangesAsync(cancellationToken);
         }
 
-        public async Task<EventLogEntry[]> GetNonPublishedEventsAsync(CancellationToken cancellationToken)
+        public async Task PublishPendingEventsAsync(IDbContextTransaction transaction, CancellationToken cancellationToken)
+        {
+            _eventLogContext.Database.UseTransaction(transaction.GetDbTransaction());
+
+            var events = await GetNonPublishedEventsAsync(cancellationToken);
+            var publishMethod = typeof(IEventBus).GetMethod(nameof(IEventBus.Publish))!;
+
+            foreach (var @event in events)
+            {
+                _logger.LogTrace("Publishing event {@EventLogEntry}", @event);
+                try
+                {
+                    @event.SetInProgress();
+                    var eventType = _eventBusSubscriptionManager.Events.FirstOrDefault(x => x.Name == @event.EventType);
+                    if (eventType is null)
+                    {
+                        _logger.LogWarning("Event Type not found for event {@EventLogEntry}", @event);
+                        throw new Exception("EventType is not Registered : " + @event.EventType);
+                    }
+                    var eventObject = JsonSerializer.Deserialize(@event.Content, eventType)!;
+                    var method = publishMethod.MakeGenericMethod(eventType);
+                    method.Invoke(_eventBus, new object[] { eventObject });
+                    @event.SetPublished();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Failed to publish event {@EventLogEntry}", @event);
+                    @event.SetFailed();
+                }
+            }
+
+            await _eventLogContext.SaveChangesAsync(cancellationToken);
+        }
+        #endregion
+
+        #region Private Methods
+        private async Task<EventLogEntry[]> GetNonPublishedEventsAsync(CancellationToken cancellationToken)
         {
             return await _eventLogContext.Events
                 .Where(x => x.Status == EventLogEntryStatus.NotPublished)
-                .AsNoTracking()
                 .ToArrayAsync(cancellationToken);
         }
-
-        public async Task SetFailedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken)
-        {
-            eventLogEntry.SetFailed();
-            if (eventLogEntry.Status == EventLogEntryStatus.PublishFailed)
-            {
-                throw new Exception("EventLogEntry is in a wrong state!");
-            }
-            _eventLogContext.Entry(eventLogEntry).State = EntityState.Modified;
-            await _eventLogContext.SaveChangesAsync(cancellationToken);
-        }
-
-        public async Task SetInProgressStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken)
-        {
-            eventLogEntry.SetInProgress();
-            if (eventLogEntry.Status == EventLogEntryStatus.InProgress)
-            {
-                throw new Exception("EventLogEntry is in a wrong state!");
-            }
-            _eventLogContext.Entry(eventLogEntry).State = EntityState.Modified;
-            await _eventLogContext.SaveChangesAsync(cancellationToken);
-        }
-
-        public async Task SetPublishedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken)
-        {
-            eventLogEntry.SetPublished();
-            if (eventLogEntry.Status == EventLogEntryStatus.Published)
-            {
-                throw new Exception("EventLogEntry is in a wrong state!");
-            }
-            _eventLogContext.Entry(eventLogEntry).State = EntityState.Modified;
-            await _eventLogContext.SaveChangesAsync(cancellationToken);
-        }
+        #endregion
     }
 }
--- Src/BuildingBlocks/EventLog/IEventLogService.cs
+++ Src/BuildingBlocks/EventLog/IEventLogService.cs
@@ -1,13 +1,11 @@
 ﻿using EventBus.Core;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace EventLog
 {
     public interface IEventLogService
     {
-        Task<EventLogEntry[]> GetNonPublishedEventsAsync(CancellationToken cancellationToken);
-        Task AddEventAsync(Event @event, CancellationToken cancellationToken);
-        Task SetPublishedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken);
-        Task SetFailedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken);
-        Task SetInProgressStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken);
+        Task AddEventAsync(Event @event, IDbContextTransaction transaction, CancellationToken cancellationToken);
+        Task PublishPendingEventsAsync(IDbContextTransaction transaction, CancellationToken cancellationToken);
     }
 }
