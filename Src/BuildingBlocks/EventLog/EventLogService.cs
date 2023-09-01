58345c926badc4861ba952c3b18895fb578af0ec
Hesam Kashefi <hesamkashefi@yahoo.com>
2023-09-01 08:50:15 +0330

    EventLogService added

---
+++ Src/BuildingBlocks/EventLog/EventLogService.cs
@@ -0,0 +1,62 @@
+﻿using EventBus.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventLog
+{
+    public class EventLogService : IEventLogService
+    {
+        private readonly EventLogContext _eventLogContext;
+
+        public EventLogService(EventLogContext eventLogContext)
+        {
+            _eventLogContext = eventLogContext;
+        }
+
+        public async Task AddEventAsync(Event @event, CancellationToken cancellationToken)
+        {
+            _eventLogContext.Events.Add(new EventLogEntry(@event));
+            await _eventLogContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<EventLogEntry[]> GetNonPublishedEventsAsync(CancellationToken cancellationToken)
+        {
+            return await _eventLogContext.Events
+                .Where(x => x.Status == EventLogEntryStatus.NotPublished)
+                .AsNoTracking()
+                .ToArrayAsync(cancellationToken);
+        }
+
+        public async Task SetFailedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken)
+        {
+            eventLogEntry.SetFailed();
+            if (eventLogEntry.Status == EventLogEntryStatus.PublishFailed)
+            {
+                throw new Exception("EventLogEntry is in a wrong state!");
+            }
+            _eventLogContext.Entry(eventLogEntry).State = EntityState.Modified;
+            await _eventLogContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task SetInProgressStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken)
+        {
+            eventLogEntry.SetInProgress();
+            if (eventLogEntry.Status == EventLogEntryStatus.InProgress)
+            {
+                throw new Exception("EventLogEntry is in a wrong state!");
+            }
+            _eventLogContext.Entry(eventLogEntry).State = EntityState.Modified;
+            await _eventLogContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task SetPublishedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken)
+        {
+            eventLogEntry.SetPublished();
+            if (eventLogEntry.Status == EventLogEntryStatus.Published)
+            {
+                throw new Exception("EventLogEntry is in a wrong state!");
+            }
+            _eventLogContext.Entry(eventLogEntry).State = EntityState.Modified;
+            await _eventLogContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
--- Src/BuildingBlocks/EventLog/IEventLogService.cs
+++ Src/BuildingBlocks/EventLog/IEventLogService.cs
@@ -1,9 +1,11 @@
-﻿namespace EventLog
+﻿using EventBus.Core;
+
+namespace EventLog
 {
     public interface IEventLogService
     {
         Task<EventLogEntry[]> GetNonPublishedEventsAsync(CancellationToken cancellationToken);
-        Task AddEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken);
+        Task AddEventAsync(Event @event, CancellationToken cancellationToken);
         Task SetPublishedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken);
         Task SetFailedStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken);
         Task SetInProgressStatusForEventAsync(EventLogEntry eventLogEntry, CancellationToken cancellationToken);
